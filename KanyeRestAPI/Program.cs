89a14f0323e269fd3f71b949a74aed948ad0a0e4
Cameron Stock <clstock33@gmail.com>
2020-12-29 20:01:17 -0600

    init commit

---
+++ KanyeRestAPI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeRestAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            Console.WriteLine(kanyeResponse);
+
+ 
+
+        }
+    }
+}
---
+++ KanyeRestAPI/QuoteGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeRestAPI
+{
+    public class QuoteGenerator
+    {
+        public static string GetKanye()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            Console.WriteLine(kanyeResponse);
+
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            return kanyeQuote;
+        }
+    }
+}
